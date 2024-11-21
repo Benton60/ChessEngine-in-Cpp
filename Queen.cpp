674dcceff3195b20f2f11076e995b0e143ee4b14
williamwalline <wpwalline23@my.trine.edu>
2024-11-21 15:36:23 -0500

    king is fully functioning

---
+++ Queen.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Trine on 11/21/2024.
+//
+
+#include "Queen.h"
---
+++ Queen.h
@@ -0,0 +1,16 @@
+//
+// Created by Trine on 11/21/2024.
+//
+
+#ifndef QUEEN_H
+#define QUEEN_H
+
+
+
+class Queen {
+
+};
+
+
+
+#endif //QUEEN_H
