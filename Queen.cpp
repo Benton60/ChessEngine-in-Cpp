c932acbab01b0a995047a0beace448658c4f23f4
williamwalline <wpwalline23@my.trine.edu>
2024-12-02 16:31:33 -0500

    Finished Queen Class/header file

--- Queen.cpp
+++ Queen.cpp
@@ -1,5 +1,137 @@
 //
-// Created by Trine on 11/21/2024.
+// Created by Will on 11/21/2024.
 //
 
 #include "Queen.h"
+Queen::Queen(Position *position, const Coordinate &location): position(position), location(location) {}
+
+void Queen::getMoves(Move moves[], int &length) {
+    //rank positive
+    for(int i = 1; i < 7; i++) {
+        Coordinate endCoordinate = Coordinate(location.file, location.rank + i);
+        if(endCoordinate.inBounds()) {
+            if(position->areSameColor(location,endCoordinate)) {
+                break;
+            }
+            if(position->getCoordinate(endCoordinate) != 0){
+                moves[length++] = Move(location, endCoordinate);
+                break;
+            }
+            moves[length++] = Move(location, endCoordinate);
+        }else {
+            break;
+        }
+    }
+    //rank negative
+    for(int i = 1; i < 7; i++) {
+        Coordinate endCoordinate = Coordinate(location.file, location.rank - i);
+        if(endCoordinate.inBounds()) {
+            if(position->areSameColor(location,endCoordinate)) {
+                break;
+            }
+            if(position->getCoordinate(endCoordinate) != 0){
+                moves[length++] = Move(location, endCoordinate);
+                break;
+            }
+            moves[length++] = Move(location, endCoordinate);
+        }else {
+            break;
+        }
+    }
+    //file positive
+    for(int i = 1; i < 7; i++) {
+        Coordinate endCoordinate = Coordinate(location.file + i, location.rank);
+        if(endCoordinate.inBounds()) {
+            if(position->areSameColor(location,endCoordinate)) {
+                break;
+            }
+            if(position->getCoordinate(endCoordinate) != 0){
+                moves[length++] = Move(location, endCoordinate);
+                break;
+            }
+            moves[length++] = Move(location, endCoordinate);
+        }else {
+            break;
+        }
+    }
+    //file negative
+    for(int i = 1; i < 7; i++) {
+        Coordinate endCoordinate = Coordinate(location.file - i, location.rank);
+        if(endCoordinate.inBounds()) {
+            if(position->areSameColor(location,endCoordinate)) {
+                break;
+            }
+            if(position->getCoordinate(endCoordinate) != 0){
+                moves[length++] = Move(location, endCoordinate);
+                break;
+            }
+            moves[length++] = Move(location, endCoordinate);
+        }else {
+            break;
+        }
+    }
+    //positive and positive
+    for(int i = 1; i < 7; i++) {
+        Coordinate endCoordinate = Coordinate(location.file + i, location.rank + i);
+        if(endCoordinate.inBounds()) {
+            if(position->areSameColor(location,endCoordinate)) {
+                break;
+            }
+            if(position->getCoordinate(endCoordinate) != 0){
+                moves[length++] = Move(location, endCoordinate);
+                break;
+            }
+            moves[length++] = Move(location, endCoordinate);
+        }else {
+            break;
+        }
+    }
+    //negative and negative
+    for(int i = 1; i < 7; i++) {
+        Coordinate endCoordinate = Coordinate(location.file - i, location.rank - i);
+        if(endCoordinate.inBounds()) {
+            if(position->areSameColor(location,endCoordinate)) {
+                break;
+            }
+            if(position->getCoordinate(endCoordinate) != 0){
+                moves[length++] = Move(location, endCoordinate);
+                break;
+            }
+            moves[length++] = Move(location, endCoordinate);
+        }else {
+            break;
+        }
+    }
+    //negative and positive
+    for(int i = 1; i < 7; i++) {
+        Coordinate endCoordinate = Coordinate(location.file - i, location.rank + i);
+        if(endCoordinate.inBounds()) {
+            if(position->areSameColor(location,endCoordinate)) {
+                break;
+            }
+            if(position->getCoordinate(endCoordinate) != 0){
+                moves[length++] = Move(location, endCoordinate);
+                break;
+            }
+            moves[length++] = Move(location, endCoordinate);
+        }else {
+            break;
+        }
+    }
+    //positive and negative
+    for(int i = 1; i < 7; i++) {
+        Coordinate endCoordinate = Coordinate(location.file + i, location.rank - i);
+        if(endCoordinate.inBounds()) {
+            if(position->areSameColor(location,endCoordinate)) {
+                break;
+            }
+            if(position->getCoordinate(endCoordinate) != 0){
+                moves[length++] = Move(location, endCoordinate);
+                break;
+            }
+            moves[length++] = Move(location, endCoordinate);
+        }else {
+            break;
+        }
+    }
+}
--- Queen.h
+++ Queen.h
@@ -1,16 +1,18 @@
 //
-// Created by Trine on 11/21/2024.
+// Created by Will on 11/21/2024.
 //
 
 #ifndef QUEEN_H
 #define QUEEN_H
-
-
+#include "Position.h"
 
 class Queen {
+    public:
+        Position* position;
+        Coordinate location;
+        Queen(Position *position, const Coordinate &location);
 
+        void getMoves(Move moves[], int &length);
 };
-
-
 
 #endif //QUEEN_H
