9bb57dcd0b43993cdcdda57c63aa0f7e1043b3f4
bento <benton.programming@gmail.com>
2024-12-05 09:28:52 -0500

    I removed some excess code  from knight class

--- Knight.cpp
+++ Knight.cpp
@@ -21,8 +21,6 @@ void Knight::getMoves(Move moves[], int &length) {
     // adds all the moves that are actually legal
     for (int i = 0; i < 8; i++) {
         if (allEndCoordinates[i].inBounds()) {
-            position->getCoordinate(location); // current position
-            position->getCoordinate(allEndCoordinates[i]); //this is the possible move
             if(!position->areSameColor(location, allEndCoordinates[i])){
                 moves[length] = Move(location, allEndCoordinates[i]);
                 length++;
