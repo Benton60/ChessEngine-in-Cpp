5ce8c59b5662e4dcfc5f9c84b0ce9128fcd4c438
bento <benton.programming@gmail.com>
2024-12-05 09:47:48 -0500

    I added documentation for the move pawn king knight and position classes

--- Pawn.cpp
+++ Pawn.cpp
@@ -10,6 +10,8 @@
 Pawn::Pawn(Position *position, const Coordinate &location): position(position), location(location) {}
 
 void Pawn::getMoves(Move moves[], int &length) {
+
+    //if the pawn is white it has different moves that the black pawns
     if (position->getCoordinate(location) > 0) { //white pawns
         Coordinate upOne = Coordinate(location.file,location.rank + 1);
         Coordinate upTwo = Coordinate(location.file,location.rank + 2);
@@ -38,12 +40,16 @@ void Pawn::getMoves(Move moves[], int &length) {
             moves[length++] = Move(location, Coordinate(position->lastMove.end.file, position->lastMove.end.rank+1));
         }
 
-
+    //we're finished with white pawn logic now it is black pawns turn
     }else { //black pawns
+        //these are the predetermined available moves for pawns
         Coordinate upOne = Coordinate(location.file,location.rank - 1);
         Coordinate upTwo = Coordinate(location.file,location.rank - 2);
         Coordinate captureLeft = Coordinate(location.file -1,location.rank - 1);
         Coordinate captureRight = Coordinate(location.file + 1,location.rank - 1);
+        //
+
+        //checks all the moves to see if they apply to this board.
         if(upOne.inBounds() && position->getCoordinate(upOne) == 0) {
             moves[length++] = Move(location, upOne);
             if(upTwo.inBounds() && position->getCoordinate(upTwo) == 0 && location.rank == 6) {
@@ -60,7 +66,6 @@ void Pawn::getMoves(Move moves[], int &length) {
 
 
         //En-pessant
-
         if(position->getCoordinate(position->lastMove.end) == 100 //Checks whether the last move was a black pawn
             && position->lastMove.end.rank - position->lastMove.start.rank == 2 //checks that the pawn moved two spaces
             && abs(position->lastMove.end.file - location.file) == 1 //checks that it is on either side of the current pawn
